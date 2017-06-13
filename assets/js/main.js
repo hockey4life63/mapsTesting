609eebb1f6fab41dea70925a6d6857d64f6c1056
hockey4life63 <Amelancon68@gmail.com>
2017-06-13 16:11:44 -0700

    added a simple distacce api call for testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
 const KEY = "AIzaSyA4h2YtjbEBrHOlD5JRpMIzsz--TPe9JW8";
+const DISTANCE_KEY = "AIzaSyDuaTBkCUIyxHEDGE6PhA4KfrChrBkTHng"
 const GEOCODE = "https://maps.googleapis.com/maps/api/geocode/json?address="
 let map = null;
+let firstClick = true;
+let origin = null;
+let dest = null;
 
 function initMap() {
     var uluru = { lat: -25.363, lng: 131.044 };
@@ -19,25 +23,67 @@ function initMap() {
 }
 $("#submit").on("click", function(e) {
         e.preventDefault()
+            // address info
         let address = $("#address").val().trim().replace(/ /g, "+");
         let city = $("#city").val().trim().replace(/ /g, "+");
         let state = $("#state").val().trim();
+        $("#address").val("")
+        $("#city").val("");
+        $("#state").val("")
+            //make query URL suffix
         let query = address + ",+" + city + ",+" + state + "&key=" + KEY
-        console.log(query)
         $.ajax({
             url: GEOCODE + query
         }).done(function(result) {
-            console.log(result)
+            //grad converted long and lat
             let position = {
-                lat: result.results[0].geometry.location.lat,
-                lng: result.results[0].geometry.location.lng
-            }
+                    lat: result.results[0].geometry.location.lat,
+                    lng: result.results[0].geometry.location.lng
+                }
+                //add new marker
             let newMarker = new google.maps.Marker({
-                position: position,
-                map: map
-            })
+                    position: position,
+                    map: map
+                })
+                //center on new marker
             map.setCenter(position)
+                //add click listener
+            google.maps.event.addListener(newMarker, "click", function() {
+                console.log(this.position)
+                    //build simple info window with address
+                let infowindow = new google.maps.InfoWindow({
+                    content: '<div id="content">' +
+                        '<div id="siteNotice">' +
+                        '</div>' +
+                        '<h1 id="firstHeading" class="firstHeading"></h1>' +
+                        '<div id="bodyContent">' +
+                        '<p>' + address.replace(/\+/g, " ") + " " + city.replace(/\+/g, " ") + state + '</p>' +
+                        '</div>' +
+                        '</div>'
+                });
+                //on first click add location to origin
+                if (firstClick) {
+                    origin = new google.maps.LatLng(this.position.lat(), this.position.lng());
+                    firstClick = false;
+                } else {
+                    //on second click add location to dest and call the distance api
+                    dest = new google.maps.LatLng(this.position.lat(), this.position.lng());
+                    var service = new google.maps.DistanceMatrixService();
+                    service.getDistanceMatrix({
+                        origins: [origin],
+                        destinations: [dest],
+                        travelMode: 'DRIVING',
+                    }, callback);
+
+                }
+
+                infowindow.open(map, newMarker)
+            })
         })
     })
-    /*https://maps.googleapis.com/maps/api/geocode/
-    json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY*/
+    //logs distance text for testing
+function callback(response, status) {
+    console.log(response.rows["0"].elements["0"].distance.text)
+}
+/*https://maps.googleapis.com/maps/api/geocode/
+json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY*/
